e6c7aee7c2c2fa4c9e1c499ed1507ed1beb1f611
Chirag Suthar <chirag@weam.ai>
2025-07-02 17:44:24 +0530

    removed subscription job queue and set nodejs storage folder in gitignore

--- nodejs/src/jobs/process.js
+++ nodejs/src/jobs/process.js
@@ -5,5 +5,4 @@ for (let identity in _processors) {
     defaultQueue.process(identity, 3, _processors[identity]);
     mailQueue.process(identity, 5, _processors[identity]);
     notificationQueue.process(identity, 5, _processors[identity]);
-    subscriptionQueue.process(identity, 5, _processors[identity]);
 }
