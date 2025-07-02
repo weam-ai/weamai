5771f084e59d1d65a91941beda066e78a139e38f
Jay Thesia <jaythesia@JDMM0052025JAYT.local>
2025-07-02 19:19:30 +0530

    uncomment seeder

--- nodejs/src/seeders/index.js
+++ nodejs/src/seeders/index.js
@@ -7,15 +7,15 @@ async function initSeed () {
         // await seedService.seedAdmin();
         // await seedService.seedNotification();
         // await seedService.seedSetting();
-        // await seedService.seedDefaultModel();
-        // await seedService.seedCustomGPT();
-        // await seedService.seedPrompt();
+        await seedService.seedDefaultModel();
+        await seedService.seedCustomGPT();
+        await seedService.seedPrompt();
         await seedService.seedOtherRolePermission();
         // await seedService.migrateUser();
         // await seedService.freeWeamApiMigration();
         // await seedService.queryLimitKeyMigration()
         // await seedService.botMigration()
-        // await seedService.seedCountry();
+        await seedService.seedCountry();
         // await seedService.seedCompanyCountryCode();
         // await seedService.seedGeneralBrain();
         await seedService.seedMigrateBlockedDomains();
