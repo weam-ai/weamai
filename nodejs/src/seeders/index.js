7ba105aa58f4ef2c2c1fe4bc06c75cff90283d05
Chirag Suthar <chirag@weam.ai>
2025-07-04 12:22:29 +0530

    remove unwanted seeder

--- nodejs/src/seeders/index.js
+++ nodejs/src/seeders/index.js
@@ -2,23 +2,16 @@ const seedService = require('../services/seeder');
 
 async function initSeed () {
     try {
-        // await seedService.seedEmailTemplate();
+        await seedService.seedEmailTemplate();
         await seedService.seedRole();
-        // await seedService.seedAdmin();
-        // await seedService.seedNotification();
-        // await seedService.seedSetting();
+        await seedService.seedAdmin();
+        await seedService.seedNotification();
+        await seedService.seedSetting();
         await seedService.seedDefaultModel();
         await seedService.seedCustomGPT();
         await seedService.seedPrompt();
         await seedService.seedOtherRolePermission();
-        // await seedService.migrateUser();
-        // await seedService.freeWeamApiMigration();
-        // await seedService.queryLimitKeyMigration()
-        // await seedService.botMigration()
-        await seedService.seedCountry();
-        // await seedService.seedCompanyCountryCode();
-        // await seedService.seedGeneralBrain();
-        await seedService.seedMigrateBlockedDomains();
+        await seedService.seedCountry();        
     } catch (error) {
         logger.error('Error in initSeed function', error);
         
