e81d25376985f1afbbc6a5494c5a1abfc1b4c18b
Jay Patel <jay@weam.ai>
2025-07-09 15:15:49 +0530

    development and prod build added for docker file

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 # build.sh
+
 # Step 1: Build the pybase_image using the pybase_docker service
-echo ":hammer: Step 1: Building pybase_image via docker-compose service pybase_docker..."
+echo " 🔨 Step 1: Building pybase_image via docker-compose service pybase_docker..."
 docker compose build pybase_docker --no-cache
+
 set -e
 # Load and export all .env variables
 set -a
 source .env
 set +a
+
+# Determine build target based on NEXT_PUBLIC_APP_ENVIRONMENT
+TARGET="production"  # default
+if [ "$NEXT_PUBLIC_APP_ENVIRONMENT" == "development" ]; then
+  TARGET="development"
+fi
+
+echo "🛠️ Building target: $TARGET based on NEXT_PUBLIC_APP_ENVIRONMENT=$NEXT_PUBLIC_APP_ENVIRONMENT"
+
+
 # Convert all .env keys into --build-arg flags
 BUILD_ARGS=$(grep -v '^#' .env | sed '/^\s*$/d' | awk -F= '{print "--build-arg " $1}')
 # Build the image
-echo ":hammer: Step 2: Building weamai-app (Next.js frontend)..."
-docker build $BUILD_ARGS -f ./nextjs/Dockerfile -t weamai-app:latest ./nextjs --no-cache
+echo "🔨 Step 2: Building weamai-app (Next.js frontend)..."
+docker build $BUILD_ARGS \
+  --target=$TARGET \
+  -f ./nextjs/Dockerfile \
+  -t weamai-app:latest \
+  ./nextjs --no-cache
+
